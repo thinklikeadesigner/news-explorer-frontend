59379d596f0c05b0a8a69472d4e06a944bbed6ec
Rebecca Burch <58955802+thinklikeadesigner@users.noreply.github.com>
2021-05-27 19:39:44 -0500

    cardslist

---
+++ src/utils/constants/constants.js
@@ -0,0 +1,12 @@
+const headerTitleBlack = 'header__title_black';
+const headerTitleWhite = '';
+const hamburger = 'header__icon_hamburger';
+const headerBgWhite= "header__container_white";
+const headerBgTransparent =  "header__container_transparent";
+const headerBgBlack =  "";
+const hiddenNavDrawer = 'navigator__drawer_hidden';
+const showNavDrawer = '';
+const hamburgerBlack = 'header__icon_hamburger-black';
+const hamburgerWhite = 'header__icon_hamburger-white';
+
+export {headerBgBlack, headerBgTransparent, headerBgWhite, headerTitleBlack, headerTitleWhite, hamburger, hamburgerBlack, hamburgerWhite, hiddenNavDrawer, showNavDrawer };
