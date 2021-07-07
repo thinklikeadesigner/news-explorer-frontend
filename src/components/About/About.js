44585460fad32c48aefe1747655f6f2e153c5d22
Rebecca Burch <58955802+thinklikeadesigner@users.noreply.github.com>
2021-07-07 11:02:41 -0500

    image issue

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './About.css';
-import avatar from '../../images/avatar.png';
+// import avatar from '../../images/avatar.png';
 
 function About() {
   return (
     <section className='about'>
       <div className='about_container'>
-        <img src={avatar} alt='avatar' className='about__pic' />
+        <img src='../../images/avatar.png' alt='avatar' className='about__pic' />
         <div className='about__text'>
           <h2 className='about__text-title'>About the author</h2>
           <p className='about__text-body'>
