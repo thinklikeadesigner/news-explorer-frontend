44911fdc3712b567820f95b1bceb8e8f6f84bfae
Rebecca Burch <58955802+thinklikeadesigner@users.noreply.github.com>
2021-07-07 10:57:17 -0500

    netlify

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-  <BrowserRouter basename="/news-explorer-frontend">
+  <BrowserRouter >
       <App/>
   </BrowserRouter>
 </React.StrictMode>,
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -2,8 +2,8 @@
 
 
 
-// const   baseUrl = process.env.NODE_ENV === 'production' ? "https://morning-paper.students.nomoreparties.site" : "http://localhost:3000";
-const   baseUrl = "https://morning-paper.students.nomoreparties.site";
+const   baseUrl = process.env.NODE_ENV === 'production' ? "https://morning-paper.students.nomoreparties.site" : "http://localhost:3000";
+// const   baseUrl = "https://morning-paper.students.nomoreparties.site";
 function checkResponse(res) {
   if (res.ok) {
     return res.json();
